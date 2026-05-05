c4e2d1f14dbe95bf1dd27c5bd2de816f68357f6f
Aly Hassan Elsokkary <68373114+Elsokkary101@users.noreply.github.com>
2026-05-05 04:54:39 +0300

    Add: sql-create-table - submission-5

---
+++ SQL/sql-create-table/submission-5.sql
@@ -0,0 +1,16 @@
+CREATE TABLE videos (
+    id  Integer,
+    name  Text,
+    created_At  Date,
+    published  Boolean
+);
+
+
+
+
+
+
+-- Do not modify below this line --
+SELECT table_name, column_name, data_type
+FROM information_schema.columns
+WHERE table_name = 'videos';
